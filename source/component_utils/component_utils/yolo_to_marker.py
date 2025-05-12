0777d0e7572e306363c0347296a72cb28d7feadb
Michael Morris <michael@aica.tech>
2025-05-12 16:18:08 +0200

    feat Change to double_array

--- source/component_utils/component_utils/yolo_to_marker.py
+++ source/component_utils/component_utils/yolo_to_marker.py
@@ -5,6 +5,8 @@ from modulo_core import EncodedState
 import clproto
 import json
 import numpy as np
+
+from rclpy.duration import Duration
 from copy import deepcopy
 
 class YoloToMarker(LifecycleComponent):
@@ -13,59 +15,42 @@ class YoloToMarker(LifecycleComponent):
 
         self.json_input = ""
         self.add_input("json_input", "json_input", String)
+        self.img_shape = np.asarray([840, 480])
 
-        self.to_find = 'cup'
-        self.add_parameter(sr.Parameter("to_find", sr.ParameterType.STRING), "thing to find and track")
+        
 
         self._state_command = sr.CartesianState()
         self.add_output("state_command", "_state_command", EncodedState, clproto.MessageType.CARTESIAN_STATE_MESSAGE)
 
-        self.fov_x = 69
-        self.fov_y = 42
+        self.to_find = 'cup'
+        self.fov = [69, 42]
+        self.goal_z = 0.0
+        self.camera_frame = [0, 0.6, 0.6, 0,0,0,0]
 
-        self.start_pos_x = 0
-        self.start_pos_y = 0.6
-        self.start_pos_z = 0
-        self.camera_height = 0.6
-
-        self.img_shape = np.asarray([840, 480])
+        
         self.add_parameter(sr.Parameter("to_find", sr.ParameterType.STRING), "thing to find and track")
-        self.add_parameter(sr.Parameter("fov_x", 69.0, sr.ParameterType.DOUBLE), "Camera FoV in X")
-        self.add_parameter(sr.Parameter("fov_y", 42.0, sr.ParameterType.DOUBLE), "Camera FoV in Y")
-        self.add_parameter(sr.Parameter("start_pos_x", 0.0, sr.ParameterType.DOUBLE), "Centre position of camera (x)")
-        self.add_parameter(sr.Parameter("start_pos_y", 0.6, sr.ParameterType.DOUBLE), "Centre position of camera (y)")
-        self.add_parameter(sr.Parameter("camera_height", 1.0, sr.ParameterType.DOUBLE), "Centre position of camera (z)")
+        self.add_parameter(sr.Parameter("fov", [69.0, 42.0], sr.ParameterType.DOUBLE_ARRAY), "Camera FoV in X")
         self.add_parameter(sr.Parameter("goal_z", 0.0, sr.ParameterType.DOUBLE), "Target position (Z)")
+        self.add_parameter(sr.Parameter("camera_frame", [0, 0.6, 0.6, 0,0,0,0], sr.ParameterType.DOUBLE_ARRAY), "Camera Position")
 
     def on_validate_parameter_callback(self, parameter: sr.Parameter) -> bool:
         # validate an incoming parameter value according to some criteria
         return True
 
     def on_configure_callback(self) -> bool:
-        self.fov_x = self.get_parameter_value("fov_x")
-        self.fov_y = self.get_parameter_value("fov_y")
-
-        self.start_pos_x = self.get_parameter_value("start_pos_x")
-        self.start_pos_y = self.get_parameter_value("start_pos_y")
-        self.camera_height = self.get_parameter_value("camera_height")
+        self.add_tf_listener()
+        self.fov = self.get_parameter_value("fov")
+        self.camera_frame = self.get_parameter_value("camera_frame")
         self.goal_z = self.get_parameter_value("goal_z")
 
-        self.camera_height
-
-        fov = np.radians(np.asarray([self.fov_x, self.fov_y]))
-        
-        self.camera_pos = np.asarray([self.start_pos_x, self.start_pos_y, self.camera_height]) # m
-        
+        fov = np.radians(np.asarray(self.fov))
 
         self.get_logger().info(
             f'fov:{tuple(f"{v:.2f}" for v in fov)},\t'
-            f'camera_height:{self.camera_height},\t'
+            f'camera_frame:{self.camera_frame},\t'
         )
 
-        
-        self.pix_xy = self.camera_height * np.tan(fov/2)
-    
-        # configuration steps before activation
+        self.pix_xy = self.camera_frame[2] * np.tan(fov/2)
         return True
 
     def on_activate_callback(self) -> bool:
@@ -101,13 +86,12 @@ class YoloToMarker(LifecycleComponent):
                 }
                 for key, coords in data.items()
             }
-            
             to_find = self.get_parameter_value("to_find")
 
             position = results[to_find + '_1']['position']
             position = (np.append(position, self.goal_z)).reshape((3,1))
-            position[:2, 0] += self.camera_pos[:2]
-                       
+            position[:2, 0] += self.camera_frame[:2]
+
             orientation = np.zeros((4, 1))
             linear_velocity = np.zeros((3, 1))
             angular_velocity = np.zeros((3, 1))
@@ -118,13 +102,7 @@ class YoloToMarker(LifecycleComponent):
             command.set_linear_velocity(linear_velocity)
             command.set_angular_velocity(angular_velocity)
 
-            self.get_logger().info(f'{command}')
-
             self._state_command = command
-
-            # position gives relative position of box to centre of frame.
-
-            self.get_logger().info(f'{results}')
 
         except Exception as e:
             self.get_logger().error(f'Error in on_step_callback: {e}')
